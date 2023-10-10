9a1a6c6de4e8e21409b80a40e3630a7ca67cbe3f
megumi <ooo.megumi9@gmail.com>
2023-10-10 20:27:27 +0900

    pictureの投稿確認機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -57,6 +57,10 @@ class PicturesController < ApplicationController
     end
   end
 
+  def confirm
+    @picture = Picture.new(picture_params)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_picture
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
